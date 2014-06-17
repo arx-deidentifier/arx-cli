8ad19d460138aec77ff56d40da8b1a7801529d25
Florian Kohlmayer <kohlmayer@gmail.com>
2014-06-17 10:55:53 +0200

    Changed scripts to launch GUI if no parameters are supplied

--- scripts/arx.bat
+++ scripts/arx.bat
@@ -1,2 +1,11 @@
 @ECHO OFF
+
+if [%1]==[] GOTO GUI
+
 "@@JAVAEXECUTABLE@@" -jar "@@INSTALLDIR@@\arx-@@VERSION@@-cli.jar" %*
+GOTO EOF
+
+:GUI
+"@@INSTALLDIR@@\arx-launcher.exe"
+
+:EOF
--- scripts/arx.sh
+++ scripts/arx.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+if [ $# -eq 0 ]; then
+    exec "@@INSTALLDIR@@/arx-launcher.run"
+    exit 0
+fi
+
 exec "@@JAVAEXECUTABLE@@" -jar "@@INSTALLDIR@@/arx-@@VERSION@@-cli.jar" $*
