2f09e1fa682b35c3c2fd87ca7fcb7f547e4a00cf
Florian Kohlmayer <kohlmayer@gmail.com>
2014-11-17 17:37:34 +0100

    Update batch file

--- scripts/arx.bat
+++ scripts/arx.bat
@@ -1,7 +1,11 @@
 @ECHO OFF
 
 if [%1]==[] GOTO GUI
+if [%1]==[-?] GOTO CLI
+if [%1]==[--help] GOTO CLI
+if [%2]==[] GOTO GUI
 
+:CLI
 "@@JAVAEXECUTABLE@@" -jar "@@INSTALLDIR@@\arx-@@VERSION@@-cli.jar" %*
 GOTO EOF
 
